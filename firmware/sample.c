9ed29df4dc93652af6d03a24e4b7dc476e523936
Haziq Ahmed Rumzy <75434700+ssj2vegeta@users.noreply.github.com>
2026-06-14 16:00:36 +0900

    Update sample.c

--- firmware/sample.c
+++ firmware/sample.c
@@ -9,18 +9,15 @@ static QueueHandle_t sample_queue;
 
 static void IRAM_ATTR sampling_isr(void *args)
 {
-    // fake sine wave simulating EMG signal
     static int t = 0;
     int sample = (int)(2048 + 1000 * sinf(2 * M_PI * 20 * t / 1000.0f));
     t++;
-    // adc_oneshot_read(adc_handle, ADC_CHANNEL_6, &sample);
 
     xQueueSendFromISR(sample_queue, &sample, NULL);
 }
 
 void sampling_init(void)
 {
-    // ADC setup
     adc_oneshot_unit_init_cfg_t init_config = {
         .unit_id = ADC_UNIT_1,
     };
@@ -32,10 +29,8 @@ void sampling_init(void)
     };
     adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_6, &chan_config);
 
-    // queue
     sample_queue = xQueueCreate(4096, sizeof(int));
 
-    // timer
     const esp_timer_create_args_t timer_args = {
         .callback = sampling_isr,
         .name = "sampling_timer"
